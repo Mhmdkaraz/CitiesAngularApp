4e3611e52c939cc32d11e5165270da0b1ba5ad5c
Mhmdkaraz <mohamedkaraz22@gmail.com>
2023-10-06 15:04:34 +0300

    Added Login UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
+import { AccountService } from './services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public accountService: AccountService, private router:Router) {}
+
+  onLogOutClicked() {
+    this.accountService.getLogout().subscribe({
+      next: (response: string) => {
+        this.accountService.currentUsername = null;
+        this.router.navigate(['/login']);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {},
+    });
+  }
+}
